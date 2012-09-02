17849047214f63dd113d1b928ad1634285bfbc35
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2012-09-02 20:47:55 +0000

    Renamed MainActivity.java to AAMeetingManager.java

--- trunk/client/src/org/mcjug/aameetingmanager/AAMeetingManager.java
+++ trunk/client/src/org/mcjug/aameetingmanager/AAMeetingManager.java
@@ -1,8 +1,8 @@
 package org.mcjug.aameetingmanager;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Vector;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -13,22 +13,17 @@ import android.view.View;
 import android.widget.TabHost;
 import android.widget.TabHost.TabContentFactory;
 
-import org.mcjug.aameetingmanager.R;
-import org.mcjug.aameetingmanager.PagerAdapter;
-
-
-public class MainActivity extends FragmentActivity implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
+public class AAMeetingManager extends FragmentActivity implements TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
 
 	private TabHost mTabHost;
 	private ViewPager mViewPager;
-	private HashMap<String, TabInfo> mapTabInfo = new HashMap<String, MainActivity.TabInfo>();
+	private HashMap<String, TabInfo> mapTabInfo = new HashMap<String, AAMeetingManager.TabInfo>();
 	private PagerAdapter mPagerAdapter;
 
 	private class TabInfo {
 		 private String tag;
          private Class<?> clss;
          private Bundle args;
-         private Fragment fragment;
          
          TabInfo(String tag, Class<?> clazz, Bundle args) {
         	 this.tag = tag;
@@ -37,6 +32,7 @@ public class MainActivity extends FragmentActivity implements TabHost.OnTabChang
          }
 
 	}
+	
 	/**
 	 * A simple factory that returns dummy views to the Tabhost
 	 * @author mwho
@@ -63,20 +59,24 @@ public class MainActivity extends FragmentActivity implements TabHost.OnTabChang
 	    }
 
 	}
+	
 	/** (non-Javadoc)
 	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
 	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		// Inflate the layout
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.aa_meeting_manager);
+		
 		// Initialize the TabHost
-		this.initialiseTabHost(savedInstanceState);
+		this.initializeTabHost(savedInstanceState);
 		if (savedInstanceState != null) {
             mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab")); //set the tab as per the saved state
         }
+		
 		// Initialize ViewPager
-		this.intialiseViewPager();
+		this.intializeViewPager();
 	}
 
     protected void onSaveInstanceState(Bundle outState) {
@@ -84,28 +84,29 @@ public class MainActivity extends FragmentActivity implements TabHost.OnTabChang
         super.onSaveInstanceState(outState);
     }
 
-    private void intialiseViewPager() {
-		List<Fragment> fragments = new Vector<Fragment>();
+    private void intializeViewPager() {
+		List<Fragment> fragments = new ArrayList<Fragment>();
 		fragments.add(Fragment.instantiate(this, SubmitMeetingFragment.class.getName()));
 		fragments.add(Fragment.instantiate(this, FindMeetingFragment.class.getName()));
-		this.mPagerAdapter  = new PagerAdapter(super.getSupportFragmentManager(), fragments);
+		this.mPagerAdapter = new PagerAdapter(super.getSupportFragmentManager(), fragments);
 
 		this.mViewPager = (ViewPager)super.findViewById(R.id.viewpager);
 		this.mViewPager.setAdapter(this.mPagerAdapter);
 		this.mViewPager.setOnPageChangeListener(this);
     }
 
-	private void initialiseTabHost(Bundle args) {
+	private void initializeTabHost(Bundle args) {
 		mTabHost = (TabHost)findViewById(android.R.id.tabhost);
         mTabHost.setup();
-        TabInfo tabInfo = null;
-        MainActivity.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec("SubmitMeeting").setIndicator("Submit Meeting"), ( tabInfo = new TabInfo("SubmitMeeting", SubmitMeetingFragment.class, args)));
+        
+        AAMeetingManager.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec("SubmitMeeting").setIndicator("Submit Meeting"));
+        TabInfo tabInfo = new TabInfo("SubmitMeeting", SubmitMeetingFragment.class, args);
         this.mapTabInfo.put(tabInfo.tag, tabInfo);
-        MainActivity.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec("FindMeeting").setIndicator("Find Meeting"), ( tabInfo = new TabInfo("FindMeeting", FindMeetingFragment.class, args)));
+        
+        AAMeetingManager.AddTab(this, this.mTabHost, this.mTabHost.newTabSpec("FindMeeting").setIndicator("Find Meeting"));
+        tabInfo = new TabInfo("FindMeeting", FindMeetingFragment.class, args);
         this.mapTabInfo.put(tabInfo.tag, tabInfo);
-        // Default to first tab
-        //this.onTabChanged("Tab1");
-        //
+        
         mTabHost.setOnTabChangedListener(this);
 	}
 
@@ -114,10 +115,8 @@ public class MainActivity extends FragmentActivity implements TabHost.OnTabChang
 	 * @param activity
 	 * @param tabHost
 	 * @param tabSpec
-	 * @param clss
-	 * @param args
 	 */
-	private static void AddTab(MainActivity activity, TabHost tabHost, TabHost.TabSpec tabSpec, TabInfo tabInfo) {
+	private static void AddTab(AAMeetingManager activity, TabHost tabHost, TabHost.TabSpec tabSpec) {
 		// Attach a Tab view factory to the spec
 		tabSpec.setContent(activity.new TabFactory(activity));
         tabHost.addTab(tabSpec);
@@ -137,7 +136,5 @@ public class MainActivity extends FragmentActivity implements TabHost.OnTabChang
 
 	public void onPageScrollStateChanged(int arg0) {
 	}
-	
-
 }
 
