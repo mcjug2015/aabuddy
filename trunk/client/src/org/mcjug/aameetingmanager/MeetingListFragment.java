dc7d7d86413030d2e699fb43dbf06d9f42889a10
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2012-10-15 00:26:20 +0000

    Initial revision

---
+++ trunk/client/src/org/mcjug/aameetingmanager/MeetingListFragment.java
@@ -0,0 +1,76 @@
+package org.mcjug.aameetingmanager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class MeetingListFragment extends ListFragment {
+	private static final String TAG = MeetingListFragment.class.getSimpleName();
+
+	private static final String NAME = "name";
+	private static final String DAY_OF_WEEK = "day_of_week";
+	private static final String START_TIME = "start_time";
+	private static final String END_TIME = "end_time";
+	private static final String ADDRESS = "address";
+	private static final String LATITUDE = "lat";
+	private static final String LONGITUDE = "long";
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		final View view = inflater.inflate(R.layout.meeting_list_fragment, container, false);
+		return view;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+        String[] from = new String[] {START_TIME, ADDRESS, LATITUDE, LONGITUDE};
+        int[] to = new int[] {R.id.startTime, R.id.address, R.id.latitude, R.id.longitude};
+        
+		try {
+	        MeetingListFragmentActivity activity = (MeetingListFragmentActivity)getActivity();
+			List<HashMap<String, String>> list = getListItems(activity.getMeetingsJson());
+	        SimpleAdapter adapter = new SimpleAdapter(getActivity(), list, R.layout.meeting_list_row, from, to);
+	        setListAdapter(adapter);
+		} catch (Exception e) {
+			Log.d(TAG, "Error setting meeting list");
+		}
+	}
+
+	protected List<HashMap<String, String>> getListItems(String meetingsJson) throws Exception {
+		List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+		if (meetingsJson != null) {
+			JSONArray meetingListJson = new JSONArray(meetingsJson);
+			JSONObject meetingJson;
+			for (int i = 0; i < meetingListJson.length(); i++) {
+				meetingJson = meetingListJson.getJSONObject(i);
+				
+				HashMap<String, String> map = new HashMap<String, String>();
+				map.put(START_TIME, meetingJson.getString(START_TIME));
+				map.put(ADDRESS, meetingJson.getString(ADDRESS));
+				map.put(LATITUDE, String.valueOf(meetingJson.getLong(LATITUDE)));
+				map.put(LONGITUDE, String.valueOf(meetingJson.getLong(LONGITUDE)));
+				
+				list.add(map);
+			}
+		}
+		return list;
+	}
+	   
+	@Override
+	public void onListItemClick(ListView listView, View view, int position, long id) {
+	}
+}
