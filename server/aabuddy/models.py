eb0dbfd42b61b33f76d31431e20f92996a23e728
victor.semenov@gmail.com <victor.semenov@gmail.com>
2012-10-20 17:12:49 +0000

    updated adming dropdowns

--- server/aabuddy/models.py
+++ server/aabuddy/models.py
@@ -12,13 +12,21 @@ class Meeting(models.Model):
     INTERNAL_TYPE_CHOICES = [(SUBMITTED, "Submitted"),
                              (APPROVED, "Approved"),
                              (REMOVED, "Removed")]
+    
+    DAY_OF_WEEK_CHOICES = [(1, "Monday"),
+                           (2, "Tuesday"),
+                           (3, "Wednesday"),
+                           (4, "Thursday"),
+                           (5, "Friday"),
+                           (6, "Saturday"),
+                           (7, "Sunday")]
 
     objects = GeoManager()
-    day_of_week = models.IntegerField(null=False, blank=False, default=1)
+    day_of_week = models.IntegerField(null=False, blank=False, default=1, choices=DAY_OF_WEEK_CHOICES)
     start_time = models.TimeField(null=False, blank=False, default=datetime.time(11, 30))
     end_time = models.TimeField(null=False, blank=False, default=datetime.time(11, 30))
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255, null=True, blank=True)
     address = models.CharField(max_length=300)
-    internal_type = models.CharField(max_length=10, default=SUBMITTED)
+    internal_type = models.CharField(max_length=10, default=SUBMITTED, choices=INTERNAL_TYPE_CHOICES)
     geo_location = models.PointField()
