8382a6c702284756bb77859a3c9ddcbee3058b22
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2012-11-09 02:21:39 +0000

    Removed intent data code

--- trunk/client/src/org/mcjug/aameetingmanager/MeetingListFragmentActivity.java
+++ trunk/client/src/org/mcjug/aameetingmanager/MeetingListFragmentActivity.java
@@ -2,25 +2,12 @@ package org.mcjug.aameetingmanager;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
 public class MeetingListFragmentActivity extends FragmentActivity {
-	private static final String TAG = MeetingListFragmentActivity.class.getSimpleName();
-	private String meetingsJson;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meeting_list);
-		
-		Bundle extras = getIntent().getExtras();
-		if (extras != null) {
-			meetingsJson = extras.getString("MEETINGS_JSON");
-			Log.d(TAG, "meetingsJson = " + meetingsJson);
-		}
     }
-
-	public String getMeetingsJson() {
-		return meetingsJson;
-	} 
 }
