2dce6594aaabb9d6d32b8f95ca15a2d6ab69ffd5
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2012-11-09 02:20:01 +0000

    Moved getting meeting list to new class

--- trunk/client/src/org/mcjug/aameetingmanager/FindMeetingFragment.java
+++ trunk/client/src/org/mcjug/aameetingmanager/FindMeetingFragment.java
@@ -1,35 +1,21 @@
 package org.mcjug.aameetingmanager;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.mcjug.aameetingmanager.MultiSpinner.MultiSpinnerListener;
 import org.mcjug.aameetingmanager.util.DateTimeUtil;
 import org.mcjug.aameetingmanager.util.LocationUtil;
 
-import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.location.Address;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -43,7 +29,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
 public class FindMeetingFragment extends Fragment {
 	private static final String TAG = FindMeetingFragment.class.getSimpleName();
@@ -131,7 +116,12 @@ public class FindMeetingFragment extends Fragment {
 	    findMeetingButton = (Button) view.findViewById(R.id.findMeetingFindButton); 
 		findMeetingButton.setOnClickListener(new OnClickListener() { 
 			public void onClick(View v) {
-				 new FindMeetingTask().execute();
+				try {
+					FindMeetingTask findMeetingTask = new FindMeetingTask(getActivity(), getFindMeetingParams());
+					findMeetingTask.execute();
+				} catch (Exception ex) {
+				    Log.d(TAG, "Error getting meetings: " + ex);
+				}
 			} 
 		}); 
 		
@@ -161,70 +151,11 @@ public class FindMeetingFragment extends Fragment {
 			endTimeButton.setText(DateTimeUtil.getTimeStr(endTimeCalendar));
 		}
 	};
-	
+		
 	private final MultiSpinnerListener daysOfWeekSpinnerListener = new MultiSpinnerListener() {
 		public void onItemsSelected(boolean[] selected) {
 		}
 	};
-	
-	private class FindMeetingTask extends AsyncTask<Void, String, String> {
-		@Override
-		protected String doInBackground(Void... arg0) {
-			HttpClient client = new DefaultHttpClient();
-			try {  
-				Activity activity = getActivity();
-				String baseUrl = getGetMeetingBaseUrl();
-				String url = baseUrl + "?" + getFindMeetingParams();
-				HttpGet request = new HttpGet(url);
-				HttpResponse httpResponse = client.execute(request);
-			    String jsonResponse = getMeetingsResponse(httpResponse);
-			    Log.d(TAG, "Find meeting jsonResponse: " + jsonResponse);
-			    if (jsonResponse == null) {
-			    	return activity.getString(R.string.noMeetingsFound);
-			    }
-			    
-			    JSONObject jsonObj = new JSONObject(jsonResponse);
-			    JSONArray jsonMeetings = jsonObj.getJSONArray("objects");
-			    if (jsonMeetings == null || jsonMeetings.length() == 0) {
-			    	return activity.getString(R.string.noMeetingsFound);
-			    }
-			    Intent intent = new Intent(activity, MeetingListFragmentActivity.class);
-			    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-			    intent.putExtra("MEETINGS_JSON", jsonMeetings.toString());
-			    startActivity(intent);
-
-			} catch (Exception e) {  
-				return "Error in find meeting: " + e;
-			} finally {
-				client.getConnectionManager().shutdown();  
-			}
-			return null;
-		}
-		
-		@Override
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
-			if (result != null) {
-				Toast.makeText(getActivity(), result, Toast.LENGTH_LONG).show();		
-			}
-		}
-	}
-	
-	private String getGetMeetingBaseUrl() {
-		Activity activity = getActivity();
-
-		StringBuilder baseUrl = new StringBuilder();
-		
-		String defaultServerBase = activity.getString(R.string.meetingServerBaseUrlDefaultValue);
-		
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity.getApplicationContext());
-		String serverBaseUrl = prefs.getString(activity.getString(R.string.meetingServerBaseUrlPreferenceName), defaultServerBase);
-		
-		baseUrl.append(serverBaseUrl);
-		baseUrl.append(activity.getString(R.string.get_meeting_url_path));
-		
-		return baseUrl.toString();
-	}
 	
 	private String getFindMeetingParams() throws Exception {
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
@@ -267,28 +198,12 @@ public class FindMeetingFragment extends Fragment {
 			}
 		}
 		
+		params.add(new BasicNameValuePair("order_by", "name"));
+
 		String paramStr = URLEncodedUtils.format(params, "utf-8");
 	    Log.d(TAG, "Find meeting request params: " + paramStr);
 
 		return paramStr;
-	}
-
-	private String getMeetingsResponse(HttpResponse httpResponse) throws Exception {
-		StringBuilder builder = new StringBuilder();
-		HttpEntity entity = httpResponse.getEntity();
-		if (entity != null) {
-			InputStream inputStream = entity.getContent();
-			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					builder.append(line);
-				}
-			} finally {
-				inputStream.close();
-			}
-		}
-		return builder.toString();
 	}
 
 	@Override
