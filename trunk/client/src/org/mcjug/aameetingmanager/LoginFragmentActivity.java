211f69c90d682fde9f5d6011db0a1cc891c70b97
alitlai@gmail.com <alitlai@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2012-11-17 18:42:31 +0000

    Added Login and Registration screens and changed submit meeting to check for username/pwd

---
+++ trunk/client/src/org/mcjug/aameetingmanager/LoginFragmentActivity.java
@@ -0,0 +1,13 @@
+package org.mcjug.aameetingmanager;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class LoginFragmentActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
---
+++ trunk/client/src/org/mcjug/aameetingmanager/RegisterFragmentActivity.java
@@ -0,0 +1,13 @@
+package org.mcjug.aameetingmanager;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class RegisterFragmentActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
