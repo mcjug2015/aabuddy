ec219f88b1b85e1b118935ca6baf8d26dbe2e380
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2012-12-09 05:06:49 +0000

    Removed imports not used

--- client/src/org/mcjug/aameetingmanager/AAMeetingManager.java
+++ client/src/org/mcjug/aameetingmanager/AAMeetingManager.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.TabHost.TabContentFactory;
