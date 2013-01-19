ffb2524a93d95278d33fda999cb880877b63ad0d
alitlai@gmail.com <alitlai@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-01-19 08:00:14 +0000

    Changed base classes to use ActionBarSherlock classes and implemented home menu item selected

--- client/src/org/mcjug/aameetingmanager/AdminPrefsActivity.java
+++ client/src/org/mcjug/aameetingmanager/AdminPrefsActivity.java
@@ -1,13 +1,32 @@
 package org.mcjug.aameetingmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class AdminPrefsActivity extends PreferenceActivity {
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+public class AdminPrefsActivity extends SherlockPreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		// Admin preferences
 		addPreferencesFromResource(R.xml.adminprefs);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				// app icon in action bar clicked; go home
+				Intent intent = new Intent(this, AAMeetingManager.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+				break;
+		}
+		return true;
 	}
 }
 
--- client/src/org/mcjug/aameetingmanager/RegisterFragmentActivity.java
+++ client/src/org/mcjug/aameetingmanager/RegisterFragmentActivity.java
@@ -1,13 +1,30 @@
 package org.mcjug.aameetingmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
-public class RegisterFragmentActivity extends FragmentActivity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+public class RegisterFragmentActivity extends SherlockFragmentActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    
+ 	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				// app icon in action bar clicked; go back to login page
+				Intent intent = new Intent(this,LoginFragmentActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+				break;
+		}
+		return true;
+	}
 }
