d755f5961766f5d78cb5141a89f139f92c61e441
victor.semenov@gmail.com <victor.semenov@gmail.com>
2013-01-22 14:08:28 +0000

    making an admin ui for user confirmations

--- server/aabuddy/admin.py
+++ server/aabuddy/admin.py
@@ -1,9 +1,16 @@
 from django.contrib.gis import admin
-from aabuddy.models import Meeting
+from aabuddy.models import Meeting, UserConfirmation
+
 
 class MeetingAdmin(admin.GeoModelAdmin):
     list_display = ('name', 'description', 'address', 'day_of_week', 'start_time', 'end_time', 'internal_type', 'creator', 'created_date', 'geo_location')
     list_filter = ('name', 'description', 'address', 'day_of_week', 'start_time', 'end_time', 'internal_type', 'creator', 'created_date',)
 
 
+class UserConfirmationAdmin(admin.GeoModelAdmin):
+    list_display = ('user', 'created_date', 'expiration_date')
+    list_filter = ('user', 'created_date', 'expiration_date')
+
+
 admin.site.register(Meeting, MeetingAdmin)
+admin.site.register(UserConfirmation, UserConfirmationAdmin)
