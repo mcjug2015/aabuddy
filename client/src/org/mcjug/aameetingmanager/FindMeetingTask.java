797109683ea88adc196e318581e5a8ae88c5afb6
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-02-02 21:32:01 +0000

    Added flag for search by id

--- client/src/org/mcjug/aameetingmanager/FindMeetingTask.java
+++ client/src/org/mcjug/aameetingmanager/FindMeetingTask.java
@@ -20,19 +20,25 @@ public class FindMeetingTask extends AsyncTask<Void, String, String> {
     private Context context;
     private String meetingParams;
 	private SharedPreferences prefs;
+	private boolean searchById = false;
 
 	public FindMeetingTask(Context context, String meetingParams) {
-        this.context = context;
-        this.meetingParams = meetingParams;
-	    prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+		this(context, meetingParams, false);
 	}
 	
+	public FindMeetingTask(Context context, String meetingParams, boolean searchById) {
+        this.context = context;
+        this.meetingParams = meetingParams;
+	    this.searchById = searchById;
+	    prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+	}
+
 	@Override
 	protected String doInBackground(Void... arg0) {
 		HttpClient client = HttpUtil.createHttpClient(); 
 		try {  
-			String baseUrl = HttpUtil.getRequestUrl(context, R.string.get_meeting_url_path);
-			String url = baseUrl + "?" + meetingParams;
+			int meetingUrlResourceId = (searchById) ? R.string.get_meeting_by_id_url_path : R.string.get_meetings_url_path;
+			String url = HttpUtil.getRequestUrl(context, meetingUrlResourceId) + "?" + meetingParams;
 		       
 			Editor editor = prefs.edit();
 			editor.putString(context.getString(R.string.meetingUrl), url);
@@ -61,10 +67,10 @@ public class FindMeetingTask extends AsyncTask<Void, String, String> {
 	}
 	
 	@Override
-	protected void onPostExecute(String result) {
-		super.onPostExecute(result);
-		if (result != null) {
-			Toast.makeText(context, result, Toast.LENGTH_LONG).show();		
+	protected void onPostExecute(String errorMsg) {
+		super.onPostExecute(errorMsg);
+		if (errorMsg != null) {
+			Toast.makeText(context, errorMsg, Toast.LENGTH_LONG).show();		
 		}
 	}
 }
