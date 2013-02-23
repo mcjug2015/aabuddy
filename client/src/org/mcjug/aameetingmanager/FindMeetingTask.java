021d717149ca1a8e2bb2abd20279227826fdfe21
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-02-23 21:57:06 +0000

    Changed code for progress dialog and error handing Changed meeting results

--- client/src/org/mcjug/aameetingmanager/FindMeetingTask.java
+++ client/src/org/mcjug/aameetingmanager/FindMeetingTask.java
@@ -1,9 +1,12 @@
 package org.mcjug.aameetingmanager;
 
+import java.util.List;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.json.JSONArray;
 import org.mcjug.aameetingmanager.util.HttpUtil;
 import org.mcjug.aameetingmanager.util.MeetingListUtil;
 
@@ -16,31 +19,48 @@ import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
-public class FindMeetingTask extends AsyncTask<Void, String, String> {
+public class FindMeetingTask extends AsyncTask<Void, String, List<Meeting>> {
     private final String TAG = getClass().getSimpleName();
     private Context context;
     private String meetingParams;
 	private SharedPreferences prefs;
-	private boolean searchById = false;
 	private ProgressDialog progressDialog;
-
-	public FindMeetingTask(Context context, String meetingParams, ProgressDialog progressDialog) {
-		this(context, meetingParams, false, progressDialog);
-	}
+	private boolean appendResults = false;
 	
-	public FindMeetingTask(Context context, String meetingParams, boolean searchById, ProgressDialog progressDialog) {
-        this.context = context;
-        this.meetingParams = meetingParams;
-	    this.searchById = searchById;
-	    this.progressDialog = progressDialog;
-	    prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+	private String errorMsg =  null;
+    private boolean isSuccess = true;
+
+	public FindMeetingTask(Context context, String meetingParams) {
+		this(context, meetingParams, context.getString(R.string.waitMsg), false);
+	}
+
+	public FindMeetingTask(Context context, String meetingParams, boolean appendResults) {
+		this(context, meetingParams, context.getString(R.string.waitMsg), appendResults);
+	}
+
+	public FindMeetingTask(Context context, String meetingParams, String waitMsg, boolean appendResults) {
+		this.context = context;
+		this.meetingParams = meetingParams;
+		this.appendResults = appendResults;
+
+	    progressDialog = new ProgressDialog(context);        
+		progressDialog.setTitle(context.getString(R.string.findMeetingProgressMsg));
+		progressDialog.setMessage(waitMsg);
+		
+		prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());	
 	}
 
 	@Override
-	protected String doInBackground(Void... arg0) {
+	protected void onPreExecute() {
+		progressDialog.show();
+	}
+
+	@Override
+	protected List<Meeting> doInBackground(Void... arg0) {
 		HttpClient client = HttpUtil.createHttpClient(); 
+		List<Meeting> meetings = null;
 		try {  
-			int meetingUrlResourceId = (searchById) ? R.string.get_meeting_by_id_url_path : R.string.get_meetings_url_path;
+			int meetingUrlResourceId = R.string.get_meetings_url_path;
 			String url = HttpUtil.getUnsecureRequestUrl(context, meetingUrlResourceId) + "?" + meetingParams;
 		       
 			Editor editor = prefs.edit();
@@ -48,34 +68,43 @@ public class FindMeetingTask extends AsyncTask<Void, String, String> {
 			editor.commit();
 		       
 			HttpGet request = new HttpGet(url);
-			HttpResponse httpResponse = client.execute(request);
-		    JSONArray jsonMeetings = MeetingListUtil.getMeetingList(httpResponse);
-		    if (jsonMeetings == null || jsonMeetings.length() == 0) {
-		    	return context.getString(R.string.noMeetingsFound);
-		    }
-		    
-		    AAMeetingApplication app = (AAMeetingApplication) context.getApplicationContext();
-		    app.setMeetingListData(jsonMeetings.toString());
-		    
-		    Intent intent = new Intent(context, MeetingListFragmentActivity.class);
-		    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-		    context.startActivity(intent);
+			HttpResponse response = client.execute(request);
+			StatusLine statusLine = response.getStatusLine();
+			if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+			    meetings = MeetingListUtil.getMeetingList(context, response);
+			} else {
+				isSuccess = false;
+		    	errorMsg = statusLine.toString();
+			}
 
 		} catch (Exception e) {  
-			return "Error in find meeting: " + e;
+			isSuccess = false;
+	    	errorMsg = "Error in find meeting: " + e;
 		} finally {
 			client.getConnectionManager().shutdown();  
 		}
-		return null;
+		
+		return meetings;
 	}
 	
 	@Override
-	protected void onPostExecute(String errorMsg) {
-		super.onPostExecute(errorMsg);
+	protected void onPostExecute(List<Meeting> meetings) {
+		if (progressDialog.isShowing()) {
+			progressDialog.dismiss();
+		}		
 		
-		progressDialog.dismiss();
-		
-		if (errorMsg != null) {
+		if (isSuccess) {
+		    AAMeetingApplication app = (AAMeetingApplication) context.getApplicationContext();
+		    if (appendResults) {
+		    	app.addMeetings(meetings);
+		    } else {
+		    	app.setMeetings(meetings);
+		    }
+		    
+		    Intent intent = new Intent(context, MeetingListFragmentActivity.class);
+		    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		    context.startActivity(intent);		
+		} else {	
 			Toast.makeText(context, errorMsg, Toast.LENGTH_LONG).show();		
 		}
 	}
