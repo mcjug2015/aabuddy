8028373944e9820d8ca57976ba1399dd47b8d448
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-02-23 21:59:49 +0000

    Refactored code

--- trunk/client/src/org/mcjug/aameetingmanager/util/MeetingListUtil.java
+++ trunk/client/src/org/mcjug/aameetingmanager/util/MeetingListUtil.java
@@ -1,21 +1,16 @@
 package org.mcjug.aameetingmanager.util;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.mcjug.aameetingmanager.Meeting;
 import org.mcjug.aameetingmanager.R;
 
 import android.content.Context;
 import android.util.Log;
-import android.widget.SimpleAdapter;
 
 public class MeetingListUtil {
     private static final String TAG = MeetingListUtil.class.getSimpleName();
@@ -25,84 +20,49 @@ public class MeetingListUtil {
 	private static final String DAY_OF_WEEK = "day_of_week";
 	private static final String START_TIME = "start_time";
 	private static final String END_TIME = "end_time";
-	private static final String TIME_RANGE = "time_range";
 	private static final String ADDRESS = "address";
 	private static final String DISTANCE = "distance";
 	private static final String LATITUDE = "lat";
 	private static final String LONGITUDE = "long";
 	
-    private static final String[] FROM = new String[] {NAME,  DAY_OF_WEEK, TIME_RANGE, ADDRESS, DISTANCE, DESCRIPTION};
-    private static final int[] TO = new int[] {R.id.meetingName, R.id.meetingDay, R.id.meetingTime, R.id.meetingAddress, R.id.meetingDistance, R.id.meetingDescription};
-
-	public static JSONArray getMeetingList(HttpResponse httpResponse) throws Exception {
-		StringBuilder builder = new StringBuilder();
-		HttpEntity entity = httpResponse.getEntity();
-		if (entity != null) {
-			InputStream inputStream = entity.getContent();
-			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					builder.append(line);
-				}
-			} finally {
-				inputStream.close();
-			}
-		}
-		
-		String jsonStr = builder.toString();		
+	public static List<Meeting> getMeetingList(Context context, HttpResponse httpResponse) throws Exception {
+		String jsonStr = HttpUtil.getContent(httpResponse);		
 	    Log.d(TAG, "Meeting list: " + jsonStr);
 	    
-	    JSONArray jsonMeetings = new JSONArray();
+		List<Meeting> meetings = new ArrayList<Meeting>();
 	    if (jsonStr != null) {
 		    JSONObject jsonObj = new JSONObject(jsonStr);
-		    jsonMeetings = jsonObj.getJSONArray("objects");
-		}
-
-		return jsonMeetings;
-	}
-	
-	public static SimpleAdapter getListAdapter(Context context, String meetingsJson) throws Exception {
-		List<HashMap<String, String>> list = getListItems(context, meetingsJson);
-		SimpleAdapter adapter = new SimpleAdapter(context, list, R.layout.meeting_list_row, FROM, TO);
-		return adapter;
-	}
-	
-	private static List<HashMap<String, String>> getListItems(Context context, String meetingsJson) throws Exception {
-		List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-		if (meetingsJson != null) {
-			JSONArray meetingListJson = new JSONArray(meetingsJson);
-			JSONObject meetingJson;
-			for (int i = 0; i < meetingListJson.length(); i++) {
-				meetingJson = meetingListJson.getJSONObject(i);
-				
-				HashMap<String, String> map = new HashMap<String, String>();
-				
-				map.put(NAME, meetingJson.getString(NAME));
-				map.put(DESCRIPTION, meetingJson.getString(DESCRIPTION));
-			
-				String[] daysOfWeek = context.getResources().getStringArray(R.array.daysOfWeekLong);
-				int dayOfWeek = meetingJson.getInt(DAY_OF_WEEK);
-				map.put(DAY_OF_WEEK, daysOfWeek[dayOfWeek - 1]);
+		    JSONArray meetingsJson = jsonObj.getJSONArray("objects");
+		    
+		    if (meetingsJson != null) {
+				JSONObject meetingJson;
+				Meeting meeting;
+				for (int i = 0; i < meetingsJson.length(); i++) {
+					meetingJson = meetingsJson.getJSONObject(i);
 					
-				String startTime = meetingJson.getString(START_TIME).substring(0, 5);
-				String endTime = meetingJson.getString(END_TIME).substring(0, 5);
-				map.put(TIME_RANGE, startTime + " - " +  endTime);
+					meeting = new Meeting();
+					meeting.setName(meetingJson.getString(NAME));
+					meeting.setDescription(meetingJson.getString(DESCRIPTION));
 				
-				map.put(ADDRESS, meetingJson.getString(ADDRESS));
-				
-				double distance = Double.parseDouble(meetingJson.getString(DISTANCE));
-				map.put(DISTANCE, String.format("%.2f", distance));
-				
-				double latitude = meetingJson.getDouble(LATITUDE);
-				map.put(LATITUDE, String.format("%.3f", latitude));
-				
-				double longitude = meetingJson.getDouble(LONGITUDE);
-				map.put(LONGITUDE, String.format("%.3f", longitude));
-				
-				list.add(map);
-			}
+					String[] daysOfWeek = context.getResources().getStringArray(R.array.daysOfWeekLong);
+					int dayOfWeek = meetingJson.getInt(DAY_OF_WEEK);
+					meeting.setDayOfWeek(daysOfWeek[dayOfWeek - 1]);
+						
+					String startTime = meetingJson.getString(START_TIME).substring(0, 5);
+					String endTime = meetingJson.getString(END_TIME).substring(0, 5);
+					meeting.setTimeRange(startTime + " - " +  endTime);
+					
+					meeting.setAddress(meetingJson.getString(ADDRESS));
+					meeting.setDistance(String.format("%.2f", Double.parseDouble(meetingJson.getString(DISTANCE))));
+					
+					meeting.setLatitude(String.format("%.2f", meetingJson.getDouble(LATITUDE)));
+					meeting.setLongitude(String.format("%.2f", meetingJson.getDouble(LONGITUDE)));
+					
+					meetings.add(meeting);
+				}
+		    }	
 		}
-		return list;
-	}
+		
+		return meetings;
+	}	
 }
