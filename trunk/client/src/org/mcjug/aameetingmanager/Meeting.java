555f0e4439b72a4a93a5f6ebb6fc26caa074e578
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-02-23 15:35:51 +0000

    Changes for list adapter

---
+++ trunk/client/src/org/mcjug/aameetingmanager/Meeting.java
@@ -0,0 +1,75 @@
+package org.mcjug.aameetingmanager;
+
+public class Meeting {
+	private String name;
+	private String description;
+	private String dayOfWeek;
+	private String timeRange;
+	private String address;
+	private String distance;
+	private String latitude;
+	private String longitude;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getDayOfWeek() {
+		return dayOfWeek;
+	}
+	
+	public void setDayOfWeek(String dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+	
+	public String getTimeRange() {
+		return timeRange;
+	}
+	public void setTimeRange(String timeRange) {
+		this.timeRange = timeRange;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getDistance() {
+		return distance;
+	}
+	
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	
+	public String getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	
+	public String getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+}
