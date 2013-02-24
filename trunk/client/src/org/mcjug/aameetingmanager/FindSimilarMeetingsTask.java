45e44cd7e0e08a9a67ca958ca0a3aed7c0768178
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-02-24 04:20:12 +0000

    Changed progress dialog

--- trunk/client/src/org/mcjug/aameetingmanager/FindSimilarMeetingsTask.java
+++ trunk/client/src/org/mcjug/aameetingmanager/FindSimilarMeetingsTask.java
@@ -13,35 +13,22 @@ import org.apache.http.protocol.HTTP;
 import org.mcjug.aameetingmanager.util.HttpUtil;
 import org.mcjug.aameetingmanager.util.MeetingListUtil;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.widget.Toast;
 
 public class FindSimilarMeetingsTask extends AsyncTask<Void, String, List<Meeting>> {
 	private final String TAG = getClass().getSimpleName();
 	private Context context;
 	private String submitMeetingParams;
 	private FindSimilarMeetingsListener listener;
-	private ProgressDialog progressDialog;
-	
 	private String errorMsg =  null;
-    private boolean isSuccess = true;
 
 	public FindSimilarMeetingsTask(Context context, String submitMeetingParams, FindSimilarMeetingsListener listener) {
 		this.context = context;
 		this.submitMeetingParams = submitMeetingParams;
 		this.listener = listener;
-        progressDialog = new ProgressDialog(context);        
 	}
 	
-	@Override
-	protected void onPreExecute() {
-		progressDialog.setTitle(context.getString(R.string.submitMeetingProgressMsg));
-		progressDialog.setMessage(context.getString(R.string.waitMsg));
-		progressDialog.show();
-	}
-
 	@Override
 	protected List<Meeting> doInBackground(Void... arg0) {
 		HttpClient client = HttpUtil.createHttpClient(); 
@@ -59,11 +46,9 @@ public class FindSimilarMeetingsTask extends AsyncTask<Void, String, List<Meetin
 			if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
 				meetings = MeetingListUtil.getMeetingList(context, response);
 			} else {
-				isSuccess = false;
 		    	errorMsg = statusLine.toString();
 			}
 		} catch (Exception ex) {
-			isSuccess = false;
 	    	errorMsg = ex.toString();
 		} finally {
 			client.getConnectionManager().shutdown();
@@ -74,20 +59,10 @@ public class FindSimilarMeetingsTask extends AsyncTask<Void, String, List<Meetin
 
 	@Override
 	protected void onPostExecute(List<Meeting> meetings) {
-		if (progressDialog.isShowing()) {
-			progressDialog.dismiss();
-		}		
-		
-		if (isSuccess) {
-			if (listener != null) {
-				listener.findSimilarMeetingsResults(meetings);
-			}
-		} else {	
-			Toast.makeText(context, errorMsg, Toast.LENGTH_LONG).show();		
-		}
+		listener.findSimilarMeetingsResults(meetings, errorMsg);
 	}
 
 	public interface FindSimilarMeetingsListener {
-		public void findSimilarMeetingsResults(List<Meeting> meetings);
+		public void findSimilarMeetingsResults(List<Meeting> meetings, String errorMsg);
 	}
 }
