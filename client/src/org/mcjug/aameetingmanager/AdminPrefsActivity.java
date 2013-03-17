1f0fb7fd54e7d498b11809fa1fb49b50981ede64
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-03-17 19:43:53 +0000

    Modified change password code

--- client/src/org/mcjug/aameetingmanager/AdminPrefsActivity.java
+++ client/src/org/mcjug/aameetingmanager/AdminPrefsActivity.java
@@ -2,14 +2,17 @@ package org.mcjug.aameetingmanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 public class AdminPrefsActivity extends SherlockPreferenceActivity {
+	
+	@SuppressWarnings("deprecation")
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
@@ -33,21 +36,9 @@ public class AdminPrefsActivity extends SherlockPreferenceActivity {
             }
         });
 		
-		Button changePasswordButton = (Button) footerView.findViewById(R.id.changePasswordButton);
+		Preference changePasswordPreference = findPreference(getString(R.string.changePasswordPreferenceKey));
 		Credentials credentials = Credentials.readFromPreferences(this);
-		if (credentials.isSet()) {
-			changePasswordButton.setEnabled(true);
-			changePasswordButton.setOnClickListener(new View.OnClickListener() {
-				public void onClick(View v) {
-					
-					Intent intent = new Intent(AdminPrefsActivity.this, ChangePasswordFragmentActivity.class);
-					intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-					startActivity(intent);
-	            }
-	        });			
-		} else {
-			changePasswordButton.setEnabled(false);
-		}	
+		changePasswordPreference.setEnabled(credentials.isSet());
 	}
 	
 	@Override
