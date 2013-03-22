e3b3961be17dfa52af60da97eea82cf1217749af
schiang703@gmail.com <schiang703@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-03-22 01:01:22 +0000

    Implement Help feature using WebView

--- client/src/org/mcjug/aameetingmanager/HelpFragment.java
+++ client/src/org/mcjug/aameetingmanager/HelpFragment.java
@@ -1,22 +1,16 @@
 package org.mcjug.aameetingmanager;
 
-import org.mcjug.aameetingmanager.HelpListAdapter;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListView;
-
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class HelpFragment extends Fragment {
 	//private static final String TAG = HelpFragment.class.getSimpleName();
-
-	private HelpListAdapter listAdapter;
-	private ExpandableListView myList;
-	private String[] questions;
-	private String[] answers;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,32 +22,25 @@ public class HelpFragment extends Fragment {
 			Bundle savedInstanceState) {
 		final View view = inflater.inflate(R.layout.help_fragment, container,
 				false);
-
-		loadHelpMessages();
-
-		// get reference to the ExpandableListView
-		myList = (ExpandableListView) view.findViewById(R.id.expandableList);
-		// create the adapter by passing your ArrayList data
-		listAdapter = new HelpListAdapter(getActivity(), questions, answers);
-		// attach the adapter to the list
-		myList.setAdapter(listAdapter);
 		
-		//collapse all Groups
-		collapseAll();
-
+		WebView webView = (WebView) view.findViewById(R.id.WebView01);
+		
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setBuiltInZoomControls(true);
+		webSettings.setJavaScriptEnabled(true);
+		
+		webView.setWebViewClient(new AABuddyWebViewClient());
+		
+		webView.loadUrl("file:///android_asset/index.html");
+		
 		return view;
 	}
-
-	private void loadHelpMessages() {
-		questions = getResources().getStringArray(R.array.questions);
-		answers = getResources().getStringArray(R.array.answers);
-	}
 	
-	private void collapseAll() {
-		int count = listAdapter.getGroupCount();
-		for (int i = 0; i < count; i++) {
-			myList.collapseGroup(i);
-		}
-	}
+	private class AABuddyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return (false);
+        }
+    }
 
 }
