3cac231ebcb3b592e84ba1d30453ea011ba2f52e
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-03-23 20:20:06 +0000

    Changed reset password

--- trunk/client/src/org/mcjug/aameetingmanager/AdminPrefsActivity.java
+++ trunk/client/src/org/mcjug/aameetingmanager/AdminPrefsActivity.java
@@ -3,14 +3,12 @@ package org.mcjug.aameetingmanager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.view.View;
-import android.widget.Button;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 public class AdminPrefsActivity extends SherlockPreferenceActivity {
-	
+
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,38 +18,27 @@ public class AdminPrefsActivity extends SherlockPreferenceActivity {
 		addPreferencesFromResource(R.xml.adminprefs);
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		View footerView = getLayoutInflater().inflate(R.layout.admin_prefs_footer, null);
-		getListView().addFooterView(footerView);
-		
-		Button resetPasswordButton = (Button) footerView.findViewById(R.id.resetPasswordButton);
-		resetPasswordButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(AdminPrefsActivity.this, ResetPasswordFragmentActivity.class);
-				intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-				
-				intent.putExtra(ResetPasswordFragmentActivity.UP_INTENT_CLASS_NAME, AdminPrefsActivity.this.getClass().getName());
-				
-				startActivity(intent);
-            }
-        });
-		
+
+		Preference resetPasswordPreference = findPreference(getString(R.string.resetPasswordPreferenceKey));
+		Intent intent = resetPasswordPreference.getIntent();
+		intent.putExtra(ResetPasswordFragmentActivity.UP_INTENT_CLASS_NAME, AdminPrefsActivity.this.getClass().getName());
+		resetPasswordPreference.setIntent(intent);
+
 		Preference changePasswordPreference = findPreference(getString(R.string.changePasswordPreferenceKey));
 		Credentials credentials = Credentials.readFromPreferences(this);
 		changePasswordPreference.setEnabled(credentials.isSet());
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-			case android.R.id.home:
-				// app icon in action bar clicked; go home
-				Intent intent = new Intent(this, AAMeetingManager.class);
-				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(intent);
-				break;
+		case android.R.id.home:
+			// app icon in action bar clicked; go home
+			Intent intent = new Intent(this, AAMeetingManager.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			break;
 		}
 		return true;
 	}
 }
-
