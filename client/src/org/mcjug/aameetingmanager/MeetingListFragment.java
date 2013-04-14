8a3aad89d6568d57902a3bde58ce4eaf323ebc40
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-04-14 02:08:46 +0000

    removed imports not used

--- client/src/org/mcjug/aameetingmanager/MeetingListFragment.java
+++ client/src/org/mcjug/aameetingmanager/MeetingListFragment.java
@@ -15,7 +15,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ListFragment;
