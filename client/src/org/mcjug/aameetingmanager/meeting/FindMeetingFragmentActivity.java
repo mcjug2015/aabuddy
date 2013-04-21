5c54522dc792c72db5ee19a855f3d55fcbb508e2
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2013-04-21 01:13:30 +0000

    Changed packages

---
+++ client/src/org/mcjug/aameetingmanager/meeting/FindMeetingFragmentActivity.java
@@ -0,0 +1,35 @@
+package org.mcjug.aameetingmanager.meeting;
+
+import org.mcjug.aameetingmanager.AAMeetingManager;
+import org.mcjug.aameetingmanager.R;
+import org.mcjug.aameetingmanager.R.layout;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+public class FindMeetingFragmentActivity extends SherlockFragmentActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.find_meeting_activity);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// app icon in action bar clicked; go home
+			Intent intent = new Intent(this, AAMeetingManager.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			break;
+		}
+		return true;
+	}
+}
---
+++ client/src/org/mcjug/aameetingmanager/meeting/MeetingAdapter.java
@@ -0,0 +1,97 @@
+package org.mcjug.aameetingmanager.meeting;
+
+import java.util.List;
+
+import org.mcjug.aameetingmanager.R;
+import org.mcjug.aameetingmanager.R.id;
+import org.mcjug.aameetingmanager.R.layout;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MeetingAdapter extends ArrayAdapter<Meeting>{
+	private List<Meeting> meetings;
+    private int selectedItem = -1;
+    
+	public MeetingAdapter(Context context, int textViewResourceId, List<Meeting> meetings) {
+		super (context, textViewResourceId, meetings);
+		this.meetings = meetings;
+	}
+
+	public void setMeetings(List<Meeting> meetings) {
+		this.meetings.clear();
+		this.meetings.addAll(meetings);
+		notifyDataSetChanged();
+	}
+
+	public void setSelectedItem(int selectedItem) {
+		this.selectedItem = selectedItem;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final Context context = getContext();
+
+		View view = convertView;
+		ViewHolder holder;
+		if (view == null) {
+
+			LayoutInflater inflater = LayoutInflater.from(context);
+			view = inflater.inflate(R.layout.meeting_list_row, null);
+			
+			holder = new ViewHolder();
+			holder.address = (TextView) view.findViewById(R.id.meetingAddress);
+			holder.day = (TextView) view.findViewById(R.id.meetingDay);
+			holder.description = (TextView) view.findViewById(R.id.meetingDescription);
+			holder.distance = (TextView) view.findViewById(R.id.meetingDistance);
+			holder.time = (TextView) view.findViewById(R.id.meetingTime);
+			holder.name = (TextView) view.findViewById(R.id.meetingName);
+
+			view.setTag(holder);
+		} else {
+		    holder = (ViewHolder) convertView.getTag();
+		}
+
+		final Meeting meeting = getItem(position);
+		if (meeting != null) {
+			holder.address.setText(meeting.getAddress());
+			holder.day.setText(meeting.getDayOfWeek());
+			holder.description.setText(meeting.getDescription());
+			holder.distance.setText(meeting.getDistance());
+			holder.time.setText(meeting.getTimeRange());
+			holder.name.setText(meeting.getName());
+		}
+		
+		if (Build.VERSION.SDK_INT < 11) {
+			RelativeLayout activeItem = (RelativeLayout) view;
+			if (position == selectedItem) {
+				activeItem.setBackgroundColor(Color.argb(0x80, 0x10, 0xb0, 0xe4));
+	
+	            // set focus on list item
+	            int top = (activeItem == null) ? 0 : activeItem.getTop();
+	            ListView listView = (ListView) parent;
+	            listView.setSelectionFromTop(position, top);
+	        } else {
+	        	activeItem.setBackgroundColor(Color.TRANSPARENT);
+	        }
+		}		
+		return view;
+	}   
+	
+	private class ViewHolder {
+		TextView address;
+		TextView day;
+		TextView description;
+		TextView distance;
+		TextView time;
+		TextView name;
+	}
+}
