4d65a5340822dfdf7bee34f81925c4ac9bfdc809
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2014-02-20 01:46:44 +0000

    Added changes for Android 2.2 for the calendar

--- client/src/org/mcjug/aameetingmanager/util/DateTimeUtil.java
+++ client/src/org/mcjug/aameetingmanager/util/DateTimeUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import org.mcjug.aameetingmanager.meeting.Meeting;
 import org.mcjug.meetingfinder.R;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -62,21 +63,20 @@ public class DateTimeUtil {
 
 		Calendar startTime = makeCalendarDate(meeting.getStartTime(), meeting.getDayOfWeekIdx());
 		Calendar endTime = makeCalendarDate(meeting.getEndTime(), meeting.getDayOfWeekIdx());
-		
+
 		Intent intent;
 		if (Build.VERSION.SDK_INT < 14) {
 			intent = new Intent(Intent.ACTION_EDIT);
 			intent.putExtra("beginTime", startTime.getTimeInMillis());
 			intent.putExtra("endTime", endTime.getTimeInMillis());
-
 		} else {
 			intent = new Intent(Intent.ACTION_INSERT);
 			intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime.getTimeInMillis());
 			intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+			intent.setData(Events.CONTENT_URI);
 		}
 
 		intent.setType("vnd.android.cursor.item/event");
-
 		intent.putExtra(Events.ALL_DAY, false);
 
 		String[] daysOfWeekCalendar = context.getResources().getStringArray(R.array.daysOfWeekCalendar);
@@ -90,10 +90,9 @@ public class DateTimeUtil {
 
 		intent.putExtra(Events.ACCESS_LEVEL, Events.ACCESS_PRIVATE);
 
-		intent.setData(Events.CONTENT_URI);
 		context.startActivity(intent);
 	}
-	
+
 	private static Calendar makeCalendarDate(Date meetingTime, int dayOfWeekIdx) {
 		// Set the meeting time (start or end time)
 		Calendar calendar = Calendar.getInstance();
