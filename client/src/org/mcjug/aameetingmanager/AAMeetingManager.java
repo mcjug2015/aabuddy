9327573e7cf969ec2e894cfde38e10254beb437f
mlsemenov <mlsemenov@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2014-03-31 03:17:03 +0000

    Fixed Recovery Date multiple small issues.

--- client/src/org/mcjug/aameetingmanager/AAMeetingManager.java
+++ client/src/org/mcjug/aameetingmanager/AAMeetingManager.java
@@ -1,9 +1,6 @@
 package org.mcjug.aameetingmanager;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Locale;
-
 import org.mcjug.aameetingmanager.authentication.Credentials;
 import org.mcjug.aameetingmanager.authentication.LoginFragmentActivity;
 import org.mcjug.aameetingmanager.authentication.LogoutDialogFragment;
@@ -13,18 +10,23 @@ import org.mcjug.aameetingmanager.meeting.SubmitMeetingFragmentActivity;
 import org.mcjug.aameetingmanager.util.DateTimeUtil;
 import org.mcjug.meetingfinder.R;
 
-import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.util.TypedValue;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 
@@ -80,38 +82,7 @@ public class AAMeetingManager extends SherlockFragmentActivity
 		});
 		
 		initRecoveryText();
-		/*
-		TextView textView = (TextView) findViewById(R.id.textViewRecoveryDate);
 		
-		if (DateTimeUtil.getRecoveryDateAllowed (getApplicationContext())){
-			Calendar recoveryDate = DateTimeUtil.getRecoveryDate(getApplicationContext());
-			if (recoveryDate != null) {
-				Calendar today = Calendar.getInstance();
-				long diffInMillisec = today.getTimeInMillis() - recoveryDate.getTimeInMillis();
-				if (diffInMillisec >= 1) {
-					long diffInDays = diffInMillisec / (24 * 60 * 60 * 1000);
-					String s;
-					s = "Congratulations on your " + diffInDays + DateTimeUtil.getOrdinalFor ((int) diffInDays) + " day\r\n of recovery";
-					textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
-					textView.setText(s);
-				}
-				else {
-					textView.setText("Select a recovery date in past");
-				}
-			}
-			else {
-				textView.setVisibility(View.VISIBLE);
-				textView.setText(R.string.recoveryDatePrompt);
-				textView = (TextView) findViewById(R.id.textViewRecoveryDateBlock);
-				textView.setVisibility(View.VISIBLE);
-			}
-		}
-		else {
-			textView.setVisibility(View.GONE);
-			textView = (TextView) findViewById(R.id.textViewRecoveryDateBlock);
-			textView.setVisibility(View.GONE);
-		}
-		*/
 	}
 
 	public void initLoginLogoutButton() {
@@ -173,42 +144,72 @@ public class AAMeetingManager extends SherlockFragmentActivity
 		
 		if (DateTimeUtil.getRecoveryDateAllowed (getApplicationContext())){
 			Calendar recoveryDate = DateTimeUtil.getRecoveryDate(getApplicationContext());
-			if (recoveryDate != null) {
-				Calendar today = Calendar.getInstance();
-				long diffInMillisec = today.getTimeInMillis() - recoveryDate.getTimeInMillis();
-				if (diffInMillisec >= 1) {
-					long diffInDays = diffInMillisec / (24 * 60 * 60 * 1000);
-					String s = "Congratulations on your " + diffInDays + 
-									DateTimeUtil.getOrdinalFor ((int) diffInDays) + " day of recovery ";
-					textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,10);
-					textView.setText(s);
-				}
-				else {
-					textView.setText("Select a recovery date in past");
-				}
+			if (buildRecoveryDateText(this.getApplicationContext(), recoveryDate, textView)) {
+				SpannableString link = makeLinkSpan("Date", new View.OnClickListener() {          
+	                @Override
+	                public void onClick(View view) {
+	                	onClickShowDatePickerDialog(view);
+	                }
+	            });
+				textView.append(link);
+				makeLinksFocusable(textView);
 			}
-			else {
-				textView.setVisibility(View.VISIBLE);
-				textView.setText(R.string.recoveryDatePrompt);
-				textView = (TextView) findViewById(R.id.textViewRecoveryDateBlock);
-				textView.setVisibility(View.VISIBLE);
-			}
+			// textView.setVisibility(View.VISIBLE);
+			ImageButton imageButton = (ImageButton) findViewById(R.id.imageButtonRecoveryDateHide);
+			imageButton.setVisibility(View.VISIBLE);
 		}
 		else {
+			
 			textView.setVisibility(View.GONE);
-			textView = (TextView) findViewById(R.id.textViewRecoveryDateBlock);
-			textView.setVisibility(View.GONE);
+			//textView = (TextView) findViewById(R.id.textViewRecoveryDateBlock);
+			//textView.setVisibility(View.GONE);
+			ImageButton imageButton = (ImageButton) findViewById(R.id.imageButtonRecoveryDateHide);
+			imageButton.setVisibility(View.GONE);
 		}
 	}
 	
-	
-	
+	public static boolean buildRecoveryDateText(Context context, Calendar recoveryDate, TextView textView) {
+		boolean needToAddDate = false;
+		textView.setVisibility(View.VISIBLE);
+		if (recoveryDate != null) {
+			Calendar today = Calendar.getInstance();
+			long diffInMillisec = today.getTimeInMillis() - recoveryDate.getTimeInMillis();
+			if (diffInMillisec >= 1) {
+				long diffInDays = diffInMillisec / (24 * 60 * 60 * 1000) + 1;
+				String s = "Congratulations on your " + diffInDays + 
+								DateTimeUtil.getOrdinalFor ((int) diffInDays) + " day of recovery";
+				textView.setText(s);
+			}
+			else {
+				textView.setText("Select a recovery date in past");
+			}
+		}
+		else {
+			textView.setText(context.getString(R.string.recoveryDatePrompt));
+			needToAddDate = true;
+		}
+		return needToAddDate;
+	}
+
+	private static void makeLinksFocusable(TextView textView) {
+		MovementMethod movementMethodm = textView.getMovementMethod();  
+        if ((movementMethodm == null) || !(movementMethodm instanceof LinkMovementMethod)) {  
+            if (textView.getLinksClickable()) {  
+            	textView.setMovementMethod(LinkMovementMethod.getInstance());  
+            }  
+        }
+	}
+
 	public static class DatePickerFragment extends DialogFragment
     					implements DatePickerDialog.OnDateSetListener {
 		@Override
 	    public android.app.Dialog onCreateDialog(Bundle savedInstanceState) {
-	        // Use the current date as the default date in the picker
-	        final Calendar c = Calendar.getInstance();
+			// If Recovery DAte was saved, use it as the default in the picker
+			Calendar c = DateTimeUtil.getRecoveryDate(getActivity());
+			if (c == null) {
+		        // If not set, use the current date
+		        c = Calendar.getInstance();
+			}
 	        int year = c.get(Calendar.YEAR);
 	        int month = c.get(Calendar.MONTH);
 	        int day = c.get(Calendar.DAY_OF_MONTH);
@@ -226,40 +227,44 @@ public class AAMeetingManager extends SherlockFragmentActivity
 	    	recoveryDate.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 	    	DateTimeUtil.setRecoveryDate( this.getActivity().getApplicationContext(), recoveryDate);
 	    	
-	    	//String myFormat = "'Recovery Date: 'MM/dd/yy"; //In which you need put here
-	     	//SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);	     	
-	     	//topTextView.setText(sdf.format(recoveryDate.getTime()));
-	    	
-	    	Calendar today = Calendar.getInstance();
-	    	topTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP,10);
-	    	
-	    	long diffInMillisec = today.getTimeInMillis() - recoveryDate.getTimeInMillis();
-			if (diffInMillisec >= 1) {
-				long diffInDays = diffInMillisec / (24 * 60 * 60 * 1000);
-				String s = "Congratulations on your " + diffInDays + 
-								DateTimeUtil.getOrdinalFor ((int) diffInDays) + " day of recovery ";
-				topTextView.setText(s);
-			}
-			else {
-				topTextView.setText("Select a recovery date in past");
-			}
+	    	buildRecoveryDateText(this.getActivity().getApplicationContext(), recoveryDate, topTextView);
+
 	    }
 	}
 	
+	// private static SpannableString makeLinkSpan(CharSequence text, View.OnClickListener listener) {
+    private static SpannableString makeLinkSpan(CharSequence text, View.OnClickListener listener) {
+        SpannableString link = new SpannableString(text);
+        link.setSpan(new ClickableString(listener), 0, text.length(), 
+            SpannableString.SPAN_INCLUSIVE_EXCLUSIVE);
+        return link;
+    }
+    
 	
+    private static class ClickableString extends ClickableSpan {  
+    	private View.OnClickListener mListener;          
+        public ClickableString(View.OnClickListener listener) {              
+            mListener = listener;  
+        }          
+        @Override  
+        public void onClick(View v) {  
+            mListener.onClick(v);  
+        }   
+    }
+    
 	public void onClickShowDatePickerDialog(View v) {
 		DialogFragment datePickerFragment = new DatePickerFragment();
 	    datePickerFragment.show(getSupportFragmentManager(), "datePicker");
 	};
 	
-	public void onClickBlockRecoveryDate (View v) {
+	public void onClickHideRecoveryDate (View v) {
 		TextView textView = (TextView) findViewById(R.id.textViewRecoveryDate);
 		textView.setVisibility(View.GONE);
-		textView = (TextView) findViewById(R.id.textViewRecoveryDateBlock);
-		textView.setVisibility(View.GONE);
-		// DateTimeUtil.resetRecoveryDate(this.getApplicationContext());
-		DateTimeUtil.setRecoveryDateAllowed(this.getApplicationContext(), false);
+		ImageButton imageButton = (ImageButton) findViewById(R.id.imageButtonRecoveryDateHide);
+		imageButton.setVisibility(View.GONE);
+		DateTimeUtil.setRecoveryDateAllowed(getApplicationContext(), false);
+		Toast.makeText(getApplicationContext(), 
+				getApplicationContext().getString(R.string.howToRecoverToastNote), 
+				Toast.LENGTH_SHORT).show();	
 	}
-	
-
 }
