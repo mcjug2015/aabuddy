5ae2735d04df84a6b453526672abadd88c74025d
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2014-04-09 22:19:18 +0000

    Changes for recovery date

--- client/src/org/mcjug/aameetingmanager/util/LocationUtil.java
+++ client/src/org/mcjug/aameetingmanager/util/LocationUtil.java
@@ -115,12 +115,14 @@ public class LocationUtil {
 		} catch (Exception e) {
 		    Log.d(TAG, "Error getting address");
 		}
+		
 		/*********** If does not work - imitate **************/
-		if (location == null) {
-			 location = new Address(Locale.getDefault());
-			 location.setLatitude(38.9867417000000032);
-			 location.setLongitude(-77.1008364999999998);
-		}
+//		if (location == null) {
+//			 location = new Address(Locale.getDefault());
+//			 location.setLatitude(38.9867417000000032);
+//			 location.setLongitude(-77.1008364999999998);
+//		}
+		
 		return location;
 	}	
 }
