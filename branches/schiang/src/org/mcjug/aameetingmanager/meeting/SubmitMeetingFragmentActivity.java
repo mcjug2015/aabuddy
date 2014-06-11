1523e54a30c0afd6e3538bf85f2ec0c6e9bc511f
schiang703@gmail.com <schiang703@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2014-06-11 11:07:37 +0000

    a branch for schiang

---
+++ branches/schiang/src/org/mcjug/aameetingmanager/meeting/SubmitMeetingFragmentActivity.java
@@ -0,0 +1,34 @@
+package org.mcjug.aameetingmanager.meeting;
+
+import org.mcjug.aameetingmanager.AAMeetingManager;
+import org.mcjug.meetingfinder.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+public class SubmitMeetingFragmentActivity extends SherlockFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.submit_meeting_activity);        
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    
+ 	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// app icon in action bar clicked; go home
+			Intent intent = new Intent(this, AAMeetingManager.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);				
+			startActivity(intent);
+			break;
+		}
+		return true;
+	}
+}
