edf5527212e335403f25d29830ddf839b243cee7
schiang703@gmail.com <schiang703@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2014-06-13 01:50:14 +0000

    schiang's branch

--- GlobalMeetingScraper/geocode/StateName/1_compile.bat
+++
@@ -1,5 +0,0 @@
-javac src\MeetingAddress.java -d bin
-javac -cp bin\json-simple-1.1.1.jar src\DistinctAddress.java -d bin
-javac src\JSONTextView.java -d bin
-javac src\GeocodeResult.java -d bin
-javac src\GeoMeetings.java -d bin
