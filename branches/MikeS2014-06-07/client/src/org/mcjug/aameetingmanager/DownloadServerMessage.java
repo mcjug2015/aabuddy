e80b2c3e9ca80c926d5f1c82091599116b158808
mlsemenov <mlsemenov@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2014-08-09 07:44:44 +0000

    Service implemented

--- branches/MikeS2014-06-07/client/src/org/mcjug/aameetingmanager/DownloadServerMessage.java
+++ branches/MikeS2014-06-07/client/src/org/mcjug/aameetingmanager/DownloadServerMessage.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
@@ -11,21 +15,36 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import org.mcjug.aameetingmanager.util.ServiceConfig;
+
 public class DownloadServerMessage extends Service {
 	static final String TAG = "DownloadServerMessage";
-	private final IBinder mBinder = new ServiceBinder();
+	
 	private int result = Activity.RESULT_CANCELED;
 	public static final String URL = "https://mcasg.org/meetingfinder/api/v1/server_message?is_active=true&format=json";
-	public static final String LOADEDSTRING = "";
+	//public static final String LOADEDSTRING = "";
 	public static final String RESULT = "result";
-	public static final String NOTIFICATION = "org.mcjug.servermessagesfinal";
-
+	public static final String NOTIFICATION = "DownloaderServiceBroadcast";
+	static final SimpleDateFormat sdf = new SimpleDateFormat(" yyyy-M-dd hh:mm:ss", Locale.US);
+	private Date mDate;
+	
+	private final IBinder mBinder = new ServiceBinder();
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		Log.v(TAG, "DownloadServerMessage onBind");
 		return mBinder;
 	}
 
+	@Override
+	  public void onCreate() {
+		  super.onCreate();
+		  mDate = new Date();
+		  Log.v(TAG, "Service onCreate:" + sdf.format(mDate));
+		  result = Activity.RESULT_OK;
+		  publishResults("starting service", result);
+	  }
+	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.v(TAG, "DownloadServerMessage onStartCommand");
@@ -53,7 +72,8 @@ public class DownloadServerMessage extends Service {
 		return result;
 	}
 	
-
+	static final String jsonSource = "{\"meta\": {\"limit\": 20, \"next\": null, \"offset\": 0, \"previous\": null, \"total_count\": 1}, \"objects\": [{\"created_date\": \"2014-08-02T10:34:08.777379\", \"id\": 7, \"is_active\": true, \"message\": \"Message to test Schiang's service\", \"resource_uri\": \"/meetingfinder/api/v1/server_message/7\", \"short_message\": \"Testing Schiang's service\", \"updated_date\": \"2014-08-02T10:34:08.777410\"}]}";
+	
 	protected void handleIntent(final Intent intent) {
 		new Thread(new Runnable() {
 			   public void run() {
@@ -61,6 +81,7 @@ public class DownloadServerMessage extends Service {
 					StringBuilder sb = new StringBuilder();
 					InputStream stream = null;
 					try {
+						
 						URL url = new URL(urlPath);
 						stream = url.openConnection().getInputStream();
 						InputStreamReader reader = new InputStreamReader(stream);
@@ -73,6 +94,10 @@ public class DownloadServerMessage extends Service {
 						result = Activity.RESULT_OK;
 						publishResults(sb.toString(), result);
 						setLoadedString(sb.toString());
+						/*
+						publishResults(jsonSource, result);
+						setLoadedString(jsonSource);
+						*/
 					} catch (Exception e) {
 						Log.v(TAG, "DownloadServerMessage exception: " + e.getCause());
 						result = Activity.RESULT_CANCELED;
@@ -92,9 +117,11 @@ public class DownloadServerMessage extends Service {
 		
 	}
 
+	
 	private void publishResults(String loadedMessage, int result) {
+		Log.v(TAG, "DownloadServerMessage publishResults");
 		Intent intent = new Intent(NOTIFICATION);
-		intent.putExtra(LOADEDSTRING, loadedMessage);
+		intent.putExtra(ServiceConfig.LOADEDSTRING, loadedMessage);
 		intent.putExtra(RESULT, result);
 		sendBroadcast(intent);
 	}
