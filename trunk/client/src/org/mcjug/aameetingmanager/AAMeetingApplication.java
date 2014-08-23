2b0f9ee9ae8228df266077e38e832ce856343366
maria.klimchak@gmail.com <maria.klimchak@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2014-08-23 01:07:54 +0000

    Added code to get meeting types

--- trunk/client/src/org/mcjug/aameetingmanager/AAMeetingApplication.java
+++ trunk/client/src/org/mcjug/aameetingmanager/AAMeetingApplication.java
@@ -3,26 +3,31 @@ package org.mcjug.aameetingmanager;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mcjug.aameetingmanager.meeting.GetMeetingTypesTask;
 import org.mcjug.aameetingmanager.meeting.MeetingListResults;
+import org.mcjug.aameetingmanager.meeting.MeetingType;
 
 import android.app.Application;
 
 public class AAMeetingApplication extends Application {
 	private MeetingListResults meetingListResults;
 	private List<Integer> meetingNotThereList = null;
-    
-	//instance 
-    private static AAMeetingApplication instance = null;
-    
+	private List<MeetingType> meetingTypes = new ArrayList<MeetingType>();
+
+	// instance 
+	private static AAMeetingApplication instance = null;
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
+
 		LocationFinder locationTask = new LocationFinder(this, null);
 		locationTask.requestLocation();
-		
-	    //set instance for our static accessor
-        instance = this;
+
+		new GetMeetingTypesTask(this, meetingTypes).execute();
+
+		// set instance for our static accessor
+		instance = this;
 	}
 
 	public MeetingListResults getMeetingListResults() {
@@ -32,26 +37,34 @@ public class AAMeetingApplication extends Application {
 	public void setMeetingListResults(MeetingListResults meetingListResults) {
 		this.meetingListResults = meetingListResults;
 	}
-	
-    /**
-     * Convenient accessor to get context, so that context doesn't need to be passed around
-     */
-    public static AAMeetingApplication getInstance() {
-        if (instance == null)
-            throw new IllegalStateException("Application not created yet!");
 
-        return instance;
-    }
-    
-    public void addToMeetingNotThereList(int meetingId) {
-    	if (meetingNotThereList == null) {
-    		meetingNotThereList = new ArrayList<Integer>();
-    	}
-    	
-    	meetingNotThereList.add(meetingId);
-    }
-    
-    public List<Integer> getMeetingNotThereList() {
-    	return meetingNotThereList;
-    }
+	public List<MeetingType> getMeetingTypes() {
+		return meetingTypes;
+	}
+
+	public void setMeetingTypes(List<MeetingType> meetingTypes) {
+		this.meetingTypes = meetingTypes;
+	}
+
+	/**
+	 * Convenient accessor to get context, so that context doesn't need to be passed around
+	 */
+	public static AAMeetingApplication getInstance() {
+		if (instance == null)
+			throw new IllegalStateException("Application not created yet!");
+
+		return instance;
+	}
+
+	public void addToMeetingNotThereList(int meetingId) {
+		if (meetingNotThereList == null) {
+			meetingNotThereList = new ArrayList<Integer>();
+		}
+
+		meetingNotThereList.add(meetingId);
+	}
+
+	public List<Integer> getMeetingNotThereList() {
+		return meetingNotThereList;
+	}
 }
