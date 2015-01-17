454145343aab3df66ee3f518c3ee0f0afea06ed4
victor.semenov@gmail.com <victor.semenov@gmail.com@dee9c63f-caa6-ad1d-c1d2-9ce14e8bf0e9>
2015-01-17 19:45:11 +0000

    even further limit work

--- trunk/client/src/org/mcjug/aameetingmanager/meeting/MeetingListFragment.java
+++ trunk/client/src/org/mcjug/aameetingmanager/meeting/MeetingListFragment.java
@@ -410,7 +410,7 @@ public class MeetingListFragment extends ListFragment {
 			String packageName = resolveInfo.activityInfo.packageName;
 			if (packageName.contains("android.email")) {
                 emailIntent.setPackage(packageName);
-            } else if (packageName.contains("mms")){
+            } else if (packageName.contains("mms") || packageName.contains("android.gm")){
                 Intent intent = new Intent();
                 intent.setComponent(new ComponentName(packageName, resolveInfo.activityInfo.name));
                 intent.setAction(Intent.ACTION_SEND);
